dca96a891e6cc317e6aca7e0586a9b848882b654
fatal_tsai <cheney1003@gmail.com>
2019-10-04 12:29:19 +0800

    first

--- app/src/main/java/com/example/readjson/MainActivity.java
+++ app/src/main/java/com/example/readjson/MainActivity.java
@@ -18,6 +18,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -28,51 +29,100 @@ import java.io.FileReader;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import android.security.NetworkSecurityPolicy;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txv;
     JSONObject jsonObject;
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Log.e("data",isCleartextTrafficPermitted());
+        txv = (TextView) findViewById(R.id.txv);
 
-        txv =(TextView)findViewById(R.id.txv);
-        txv.setText(loadJSONFromAsset(getApplicationContext()));
+        new GoodTask().execute("http://192.168.151.10:3000/home/=getvideo");
+
+
+        //loadJSONFromNet();
+        //txv.setText(loadJSONFromAsset(getApplicationContext()));
+        //txv.setText(fetchJSON());
+            /*
         try {
             jsonObject = new JSONObject(loadJSONFromAsset(getApplicationContext()));
         }catch (JSONException e){
             e.printStackTrace();
 
         }
+        */
+
     }
 
-    public String loadJSONFromAsset(Context context) {
-        String json = null;
-        try {
-            InputStream is = context.getAssets().open("work=getphoto.json");
-
-            int size = is.available();
-
-            byte[] buffer = new byte[size];
-
-            is.read(buffer);
-
-            is.close();
-
-            json = new String(buffer, "UTF-8");
 
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
+
+
+//Ref :http://codenamker.pixnet.net/blog/post/161818334-%E3%80%90android%E3%80%91%E7%95%B0%E6%AD%A5%E5%9F%B7%E8%A1%8C%E7%B7%92asynctask-android-studio
+    class GoodTask extends AsyncTask<String, Integer, String> {
+        // <傳入參數, 處理中更新介面參數, 處理後傳出參數>
+        private static final int TIME_OUT = 1000;
+
+        String jsonString1 = "";
+
+        protected void onPreExecute() {
         }
-        Log.e("data", json);
-        return json;
 
+        @Override
+        protected String doInBackground(String... countTo) {
+            // TODO Auto-generated method stub
+            // 再背景中處理的耗時工作
+            try {
+                HttpURLConnection conn = null;
+                URL url = new URL(countTo[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.connect();
+                // 讀取資料
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        conn.getInputStream(), "UTF-8"));
+                jsonString1 = reader.readLine();
+                reader.close();
+
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+
+                }
+                if (jsonString1.equals("")) {
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "網路中斷" + e;
+            }
+            //Log.e("data",jsonString1);
+            return jsonString1;
+        }
+        public void onPostExecute(String result )
+        { super.onPreExecute();
+            // 背景工作處理完"後"需作的事
+            txv.setText("JSON:\r\n"+ result);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // TODO Auto-generated method stub
+            super.onProgressUpdate(values);
+            // 背景工作處理"中"更新的事
+
+        }
     }
+
+
 }
